41abf23853d9d413321c1c9989207b9238162be9
kiganyamburu <mburukiganya@gmail.com>
2024-12-03 21:44:25 +0300

    nothing

--- backend/ai_blog_app/ai_blog_app/settings.py
+++ backend/ai_blog_app/ai_blog_app/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'ai_blog_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'testdb',
+        'USER': 'root',
+        'PASSWORD': 'think_and_grow_rich!',
+        'HOST': 'localhost',  # or the hostname where your MySQL server is running
+        'PORT': '3306',      # or the port on which your MySQL server is listening
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
