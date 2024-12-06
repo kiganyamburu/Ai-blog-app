09d7563aa6f9a6c7b53997768c1edd2fed5ca962
kiganyamburu <mburukiganya@gmail.com>
2024-12-06 15:44:17 +0300

    password function

--- backend/ai_blog_app/blog_generator/views.py
+++ backend/ai_blog_app/blog_generator/views.py
@@ -13,6 +13,11 @@ def user_signup(request):
         email = request.POST['email']
         password = request.POST['password']
         repeatPassword = request.POST['repeatPassword']
+        if password == repeatPassword:
+            pass
+        else:
+            error_messages = 'Passwords do not match'
+            return render(request, 'signup.html', {'error_messages': error_messages})
     return render(request, 'signup.html')
 
 def user_logout(request):
